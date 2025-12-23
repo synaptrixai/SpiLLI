4c59fd2b924d9639763ff7cf8dd5ab74b2adbd61
synaptrixai <tech@synaptrix.org>
2025-12-23 12:58:22 -0500

    Update SpiLLI_FirstUse.py

--- Tutorials/SpiLLI_FirstUse.py
+++ Tutorials/SpiLLI_FirstUse.py
@@ -1,17 +1,26 @@
 #%% Import Spin
 from SpiLLI.SpinLLI import Spin
+import asyncio
 # %% Initialize the network module
 spin = Spin("SpiLLI_Community.pem")
 # %% Request a AI model
 llm = spin.request({"model":"Gpt-Oss-20B"})
+# %% Write a callback to execute on response
+def on_done(task: asyncio.Task):
+    try:
+        result = task.result()
+        print("Result:", result)
+    except Exception as e:
+        print("Task failed:", e)
 # %% Run your queries with a prompt and query
-res = await llm.run({"prompt":"You are a helpful AI programmer","query":"Can you show me how to multiply two matrices using numpy"})
+run1 = llm.run({"prompt":"You are a helpful AI programmer","query":"Can you show me how to multiply two matrices using numpy"})
+task = asyncio.create_task(run1)
+task.add_done_callback(on_done)
 #%%
-print(res)
 # %% You can request another model in parallel
 # llm1 = spin.request({"model":"deepseek-r1:32b"})
-# %% Run another query
-# res = llm1.run({"prompt":"You are a helpful AI programmer","query":"Can you show me how to multiply two matrices using numpy"})
+# %% Run another query using a await pattern (works if running in a jupyter notebook or as part of a async function)
+# res = await llm1.run({"prompt":"You are a helpful AI programmer","query":"Can you show me how to multiply two matrices using numpy"})
 # %%
 # print(res)
 # %%
